c9f6c95409420ffb72766ebd3a53950bf3f61724
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-20 21:04:50 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,7 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default';
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components'{
+    export interface DefaultTheme extends ThemeType {}
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  );
+}
