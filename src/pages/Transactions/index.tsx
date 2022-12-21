ec1ce0bfca077a0c570ba93d999a0f631053cb91
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-21 18:00:29 -0300

    Componente SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,42 @@
 import { Summary } from "../../Summary";
 import { Header } from "../../components/Header";
+import { SearchForm } from "./components/SeachForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
-      <Summary/>
+      <Summary />
+
+      <TransactionsContainer>
+      <SearchForm/>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                - R$ 59,90 
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,1 +1,40 @@
-import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import { Transactions } from "./index";
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme["gray-700"]};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighLightProps {
+    variant: 'income' | 'outcome';
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+
+color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+
+`;
