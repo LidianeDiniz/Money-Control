ef48368b725b1222f15f88f6095cc8e41790da72
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-29 21:12:34 -0300

    Criando nova transação

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -4,6 +4,9 @@ import { ArrowCircleDown, X, ArrowCircleUp } from 'phosphor-react';
 import *as z from 'zod';
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { api } from "../../lib/axios";
+import { useContext } from 'react';
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 
 const newTransactionFormSchema = z.object({
@@ -16,11 +19,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-
+const {createTransaction} = useContext(TransactionsContext)
   const {
     control,
     register, 
     handleSubmit,
+    reset,
     formState: {isSubmitting}
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
@@ -30,8 +34,17 @@ export function NewTransactionModal() {
   })
 
  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(data);
+  const {description, price, category, type} = data;
+
+  await createTransaction({
+    description,
+    price,
+    category,
+    type,
+  })
+ 
+
+ reset()
   }
   return (
     <Dialog.Portal>
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,15 +11,25 @@ interface Transaction {
   }
 
   
+interface CreateTransactionInput{
+  description: string;
+  price: number;
+  category: string;
+  type: 'income'  | 'outcome';
+}
+
+  
 interface TransactionContextType{
    transactions: Transaction[];
    fetchTransactions: (query?: string) => Promise<void>;
+   createTransaction : (data: CreateTransactionInput) => Promise<void>;
 
 }
 
 interface TransactionsProviderProps{
    children: ReactNode;
 }
+
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
@@ -29,13 +39,30 @@ const [ transactions, setTransactions] = useState<Transaction[]>([]);
 
 
   async function fetchTransactions(query?: string){
-   const response = await api.get('transactions', {
+   const response = await api.get('/transactions', {
     params: {
+      _sort: 'createdAt',
+      _order: 'desc',
       q: query,
     }
    })
 
     setTransactions(response.data);
+  }
+
+  async function createTransaction( data: CreateTransactionInput){
+    const {description, price, category, type} = data;
+
+    const response = await api.post('/transactions',{
+      description,
+      price,
+      category,
+      type, 
+      createdAt: new Date()
+   
+    })
+   
+    setTransactions(state => [response.data, ...state]);
   }
 
   useEffect(() => {    
@@ -48,6 +75,7 @@ const [ transactions, setTransactions] = useState<Transaction[]>([]);
         <TransactionsContext.Provider value={{ 
           transactions,
           fetchTransactions,
+          createTransaction,
           
           }}>
             {children}
